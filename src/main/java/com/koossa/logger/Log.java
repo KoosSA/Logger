66aeb4dfba6aa265385d3e3f6649601fa9475117
Koos <kkposbus@gmail.com>
2021-05-31 13:37:16 +0200

    Added multithreaded support.

--- src/main/java/com/koossa/logger/Log.java
+++ src/main/java/com/koossa/logger/Log.java
@@ -1,132 +1,132 @@
 package com.koossa.logger;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Simple logger library.
- * Call init() to start and dispose() to stop and save file.
- * info(), debug() and error() called between init() and dispose().
+ * Simple logger library. <br>
+ * Log should be initialised and disposed individually for each {@link Thread}. <br>
+ * Call init() to start and dispose() to stop and save file. <br>
+ * info(), debug() and error() can be called between init() and dispose(). <br>
  * @author KoosSA
  *
  */
 public class Log {
 	
-	private static File logFolder;
-	private static int maxFiles = 50;
-	private static String fileExt = ".log";
-	private static FileWriter writer;
-	private static File logFile;
-	private static String name;
-	private static boolean debug = false;
-	private static StringBuilder builder;
-	private static SimpleDateFormat format = new SimpleDateFormat("[HH_mm_ss]");
+	private static Map<String, LogInstance> instances = new HashMap<String, LogInstance>();
+	private static File folder;
+	private static boolean debug;
 	
 	/**
-	 * Initialises the Logger and prepare for receiving log commands.
+	 * Initialises the logger for the current {@link Thread}.
 	 * @param folder
 	 * @param debug
 	 */
 	public static void init(File folder, boolean debug) {
-		logFolder = folder;
+		Log.folder = folder;
 		Log.debug = debug;
-		if (!folder.exists()) {
-			folder.mkdirs();
+		String name = Thread.currentThread().getName();
+		if (instances.containsKey(name)) {
+			return;
 		}
-		if (folder.list().length >= maxFiles) {
-			int numToDelete = folder.list().length - maxFiles + 1;
-			for (int i = 0; i < numToDelete; i++) {
-				File file = folder.listFiles()[i];
-				file.deleteOnExit();
-			}
-		}
-		builder = new StringBuilder();
-		generateFile();
+		LogInstance instance = new LogInstance();
+		instance.init(new File(folder, name), debug);
+		instances.put(name, instance);
 	}
 	
 	/**
-	 * Disposes the Logger and save the log file.
+	 * Prints an information message on the logger of the current {@link Thread}.
+	 * @param obj
+	 * @param message
+	 */
+	public static void info(Object obj, String message) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
+			Log.init(folder, debug);
+		}
+		instances.get(name).info(obj, message);
+	}
+	
+	/**
+	 * Prints an information message on the logger of the current {@link Thread}.
+	 * @param clazz
+	 * @param message
+	 */
+	public static void info(Class<?> clazz, String message) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
+			Log.init(folder, debug);
+		}
+		instances.get(name).info(clazz, message);
+	}
+	
+	/**
+	 * Prints an error message on the logger of the current {@link Thread}.
+	 * @param obj
+	 * @param message
+	 */
+	public static void error(Object obj, String message) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
+			Log.init(folder, debug);
+		}
+		instances.get(name).error(obj, message);
+	}
+	
+	/**
+	 * Prints an error message on the logger of the current {@link Thread}.
+	 * @param clazz
+	 * @param message
+	 */
+	public static void error(Class<?> clazz, String message) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
+			Log.init(folder, debug);
+		}
+		instances.get(name).error(clazz, message);
+	}
+	
+	/**
+	 * Prints an debug message on the logger of the current {@link Thread}. <br>
+	 * Debugging should be enabled on the {@link Thread}, otherwise nothing will be printed.
+	 * @param obj
+	 * @param message
+	 */
+	public static void debug(Object obj, String message) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
+			Log.init(folder, debug);
+		}
+		instances.get(name).debug(obj, message);
+	}
+	
+	/**
+	 * Prints an debug message on the logger of the current {@link Thread}. <br>
+	 * Debugging should be enabled on the {@link Thread}, otherwise nothing will be printed.
+	 * @param clazz
+	 * @param message
+	 */
+	public static void debug(Class<?> clazz, String message) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
+			Log.init(folder, debug);
+		}
+		instances.get(name).debug(clazz, message);
+	}
+	
+	/**
+	 * Disposes the {@link Log} on the current {@link Thread}. <br>
+	 * Saves the log file. <br>
+	 * Should be done once per {@link Thread}.
 	 */
 	public static void dispose() {
-		try {
-			writer = new FileWriter(logFile);
-			writer.write(builder.toString());
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	/**
-	 * Sets the max number of log files to keep on device. Default 50.
-	 * Should be called before init().
-	 * @param maxFiles
-	 */
-	public static void setMaxFiles(int maxFiles) {
-		Log.maxFiles = maxFiles;
-	}
-
-	/**
-	 * Sets the file extension of log files. Defaults to "example.log".
-	 * Should be called before init().
-	 * @param fileExt
-	 */
-	public static void setFileExt(String fileExt) {
-		Log.fileExt = fileExt;
-	}
-	
-	private static void generateFile() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd [HH_mm_ss]");
-		name = sdf.format(Calendar.getInstance().getTime()) + fileExt;
-		logFile = new File(logFolder, name);
-	}
-	
-	public static void debug(Class<?> clazz, String message) {
-		if (!debug) {
+		String name = Thread.currentThread().getName();
+		if (!instances.containsKey(name)) {
 			return;
 		}
-		message = format.format(Calendar.getInstance().getTime()) + " DEBUG: {" + clazz.getName() + "} >>> " + message;
-		System.out.println(message);
-		builder.append(message + "\n");
+		instances.get(name).dispose();
+		instances.remove(name);
 	}
-	
-	public static void debug(Object obj, String message) {
-		if (!debug) {
-			return;
-		}
-		message = format.format(Calendar.getInstance().getTime()) + " DEBUG: {" + obj.getClass().getName() + "} >>> " + message;
-		System.out.println(message);
-		builder.append(message + "\n");
-	}
-	
-	public static void info(Class<?> clazz, String message) {
-		message = format.format(Calendar.getInstance().getTime()) + " INFO: {" + clazz.getName() + "} >>> " + message;
-		System.out.println(message);
-		builder.append(message + "\n");
-	}
-	
-	public static void info(Object obj, String message) {
-		message = format.format(Calendar.getInstance().getTime()) + " INFO: {" + obj.getClass().getName() + "} >>> " + message;
-		System.out.println(message);
-		builder.append(message + "\n");
-	}
-	
-	public static void error(Class<?> clazz, String message) {
-		message = format.format(Calendar.getInstance().getTime()) + " ERROR: {" + clazz.getName() + "} >>> " + message;
-		System.err.println(message);
-		builder.append(message + "\n");
-	}
-	
-	public static void error(Object obj, String message) {
-		message = format.format(Calendar.getInstance().getTime()) + " ERROR: {" + obj.getClass().getName() + "} >>> " + message;
-		System.err.println(message);
-		builder.append(message + "\n");
-	}
-	
-	
-	
+
 }
---
+++ src/main/java/com/koossa/logger/LogInstance.java
@@ -0,0 +1,132 @@
+package com.koossa.logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Simple instance of a logger. <br>
+ * There exists one instance per {@link Thread}.
+ * @author KoosSA
+ *
+ */
+public class LogInstance {
+	
+	private File logFolder;
+	private int maxFiles = 50;
+	private String fileExt = ".log";
+	private FileWriter writer;
+	private File logFile;
+	private String name;
+	private boolean debug = false;
+	private StringBuilder builder;
+	private SimpleDateFormat format = new SimpleDateFormat("[HH_mm_ss]");
+	
+	/**
+	 * Initialises the Logger and prepare for receiving log commands.
+	 * @param folder
+	 * @param debug
+	 */
+	protected void init(File folder, boolean debug) {
+		logFolder = folder;
+		
+		this.debug = debug;
+		if (!logFolder.exists()) {
+			logFolder.mkdirs();
+		}
+		if (logFolder.list().length >= maxFiles) {
+			int numToDelete = logFolder.list().length - maxFiles + 1;
+			for (int i = 0; i < numToDelete; i++) {
+				File file = logFolder.listFiles()[i];
+				file.deleteOnExit();
+			}
+		}
+		builder = new StringBuilder();
+		generateFile();
+	}
+	
+	/**
+	 * Disposes the Logger and save the log file.
+	 */
+	protected void dispose() {
+		try {
+			writer = new FileWriter(logFile);
+			writer.write(builder.toString());
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * Sets the max number of log files to keep on device. <br> Default 50. <br>
+	 * Should be called before init().
+	 * @param maxFiles
+	 */
+	protected void setMaxFiles(int maxFiles) {
+		this.maxFiles = maxFiles;
+	}
+
+	/**
+	 * Sets the file extension of log files.<br> Defaults to "example.log". <br>
+	 * Should be called before init().
+	 * @param fileExt
+	 */
+	protected void setFileExt(String fileExt) {
+		this.fileExt = fileExt;
+	}
+	
+	protected void generateFile() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd [HH_mm_ss]");
+		name = sdf.format(Calendar.getInstance().getTime()) + fileExt;
+		logFile = new File(logFolder, name);
+	}
+	
+	protected void debug(Class<?> clazz, String message) {
+		if (!debug) {
+			return;
+		}
+		message = format.format(Calendar.getInstance().getTime()) + " DEBUG: {" + clazz.getName() + "} >>> " + message;
+		System.out.println(message);
+		builder.append(message + "\n");
+	}
+	
+	protected void debug(Object obj, String message) {
+		if (!debug) {
+			return;
+		}
+		message = format.format(Calendar.getInstance().getTime()) + " DEBUG: {" + obj.getClass().getName() + "} >>> " + message;
+		System.out.println(message);
+		builder.append(message + "\n");
+	}
+	
+	protected void info(Class<?> clazz, String message) {
+		message = format.format(Calendar.getInstance().getTime()) + " INFO: {" + clazz.getName() + "} >>> " + message;
+		System.out.println(message);
+		builder.append(message + "\n");
+	}
+	
+	protected void info(Object obj, String message) {
+		message = format.format(Calendar.getInstance().getTime()) + " INFO: {" + obj.getClass().getName() + "} >>> " + message;
+		System.out.println(message);
+		builder.append(message + "\n");
+	}
+	
+	protected void error(Class<?> clazz, String message) {
+		message = format.format(Calendar.getInstance().getTime()) + " ERROR: {" + clazz.getName() + "} >>> " + message;
+		System.err.println(message);
+		builder.append(message + "\n");
+	}
+	
+	protected void error(Object obj, String message) {
+		message = format.format(Calendar.getInstance().getTime()) + " ERROR: {" + obj.getClass().getName() + "} >>> " + message;
+		System.err.println(message);
+		builder.append(message + "\n");
+	}
+	
+	
+	
+}
